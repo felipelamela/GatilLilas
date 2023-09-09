6b41d79aa3419297015639246fb94887fcbbe0b0
Felipe Lamela <fe-fl@outlook.com>
2023-09-09 17:51:22 -0300

    chore: add Button Component

---
+++ Components/GlobalComponents/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+
+interface ButtonTypes {
+  text: string,
+}
+
+const Button = () => {
+  return (
+    <>
+      <button>Button</button>
+
+    </>
+  )
+}
+
+export default Button
