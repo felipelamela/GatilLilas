e4423fa1eb48c611fcd1936f726fa4e79745a38a
Felipe Lamela <fe-fl@outlook.com>
2023-09-10 14:48:43 -0300

    chore: add sobreraca component

---
+++ Components/SectionsComponents/SobreRaca/SobreRaca.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SobreRaca = () => {
+  return (
+    <>
+      <section>
+        
+
+      </section>
+      <style jsx>{``}</style>
+    </>
+  )
+}
+
+export default SobreRaca
