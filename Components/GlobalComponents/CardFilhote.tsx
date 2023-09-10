a699afdba2c7a6638262c35f5ecc780244187b2a
Felipe Lamela <fe-fl@outlook.com>
2023-09-10 14:47:15 -0300

    chore: add cardFilhote  component

--- Components/GlobalComponents/CardFilhote.tsx
+++ Components/GlobalComponents/CardFilhote.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const CardFilhote = () => {
   return (
-    <div>CardFilhote</div>
+    <>
+      <figure>
+        <img src="/image/img-card1.jpg" alt="" />
+        <figcaption className='titleCard'>Red - Macho</figcaption>
+        <figcaption className='textCard'>Ninhada: 02/01/2023</figcaption>
+        <figcaption className='textCard'>Pais: Buzz e Milk</figcaption>
+      </figure>
+      <style jsx>{`
+          img{
+            max-width:180px
+          }
+
+      `}</style>
+
+    </>
   )
 }
 
