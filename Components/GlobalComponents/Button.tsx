e36df7d0db51498376513d452357dcb7c647dbf5
Felipe Lamela <fe-fl@outlook.com>
2023-09-10 14:46:59 -0300

    chore: add button component

--- Components/GlobalComponents/Button.tsx
+++ Components/GlobalComponents/Button.tsx
@@ -6,10 +6,26 @@ interface ButtonTypes {
   text: string,
 }
 
-const Button = () => {
+const Button: React.FC<ButtonTypes> = ({ text }) => {
   return (
     <>
-      <button>Button</button>
+      <button>{text}</button>
+
+      <style jsx>{`
+        button{
+          border:none;
+          display:inline;
+          margin:.5rem;
+          padding:.8rem 1.6rem;
+          cursor: pointer;
+          border-radius: 6px;
+          background:red;
+          border:1px sold, red;
+          color: white;
+        }
+      
+      
+      `}</style>
 
     </>
   )
