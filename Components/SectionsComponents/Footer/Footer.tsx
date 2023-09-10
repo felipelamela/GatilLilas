15bc183d87eeaf4e9a924c056c470aaa0317ead4
Felipe Lamela <fe-fl@outlook.com>
2023-09-10 14:48:01 -0300

    chore: add footer component

---
+++ Components/SectionsComponents/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <section>
+        footer
+
+      </section>
+      <style jsx>{``}</style>
+    </>
+  )
+}
+
+export default Footer
