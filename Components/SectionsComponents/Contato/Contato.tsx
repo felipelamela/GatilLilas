576bb50724a3489d7480294d6644617d30d2efec
Felipe Lamela <fe-fl@outlook.com>
2023-09-10 14:47:39 -0300

    chore: add contato component

---
+++ Components/SectionsComponents/Contato/Contato.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contato = () => {
+  return (
+    <>
+      <section>
+        contato
+
+      </section>
+      <style jsx>{``}</style>
+    </>
+  )
+}
+
+export default Contato
